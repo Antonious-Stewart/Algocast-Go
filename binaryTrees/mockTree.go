4a90979f42d70d8df60839cb7caaffaaa4ef0296
Antonious Stewart <antonious.stewart@gmail.com>
2024-09-17 08:31:03 -0400

    change location of the mock root

---
+++ binaryTrees/mockTree.go
@@ -0,0 +1,23 @@
+package binary_trees
+
+var MockRoot = Node{
+	Data: 7,
+	Left: &Node{
+		Data: 23,
+		Left: &Node{
+			Data: 5,
+		},
+		Right: &Node{
+			Data: 4,
+		},
+	},
+	Right: &Node{
+		Data: 3,
+		Left: &Node{
+			Data: 18,
+		},
+		Right: &Node{
+			Data: 21,
+		},
+	},
+}
--- binaryTrees/preOrder_test.go
+++ binaryTrees/preOrder_test.go
@@ -6,30 +6,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var MockRoot = Node{
-	Data: 7,
-	Left: &Node{
-		Data: 23,
-		Left: &Node{
-			Data: 5,
-		},
-		Right: &Node{
-			Data: 4,
-		},
-	},
-	Right: &Node{
-		Data: 3,
-		Left: &Node{
-			Data: 18,
-		},
-		Right: &Node{
-			Data: 21,
-		},
-	},
-}
-
 func TestPreOrderSearch(t *testing.T) {
-	printedTree := PreOrderSearch(&root)
+	printedTree := PreOrderSearch(&MockRoot)
 
 	assert.Equal(t, []int{7, 23, 5, 4, 3, 18, 21}, printedTree)
 }
